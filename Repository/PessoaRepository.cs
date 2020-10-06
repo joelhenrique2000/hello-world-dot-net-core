3bfb128d80163d1f227ca34f0a034efea3ddd2d5
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-05 22:38:39 -0300

    criado o CRUD no repository

--- Repository/PessoaRepository.cs
+++ Repository/PessoaRepository.cs
@@ -10,21 +10,50 @@ using System.Text;
 
 namespace hello_world_console_dotnet_core.Repository {
     class PessoaRepository {
-        //public IEnumerable<Pessoa> ListALL() {
-            
-        //    using var connection = new SqlConnection("Server=localhost; Database = ESTUDO1; Trusted_Connection = true");
 
-        //    var pessoaData = connection.Query<Pessoa>("SELECT * FROM PESSOA");
+        private readonly string _connectionString;
 
-        //    return pessoaData;
-        //}
+        public PessoaRepository() {
+            _connectionString = "Server=localhost\\SQLEXPRESS; Database = ESTUDO1; Trusted_Connection = true";
+        }
+
+        public IEnumerable<Pessoa> ListALL() {
+
+            using var connection = new SqlConnection(_connectionString);
+
+            var pessoaData = connection.Query<Pessoa>("SELECT * FROM PESSOA");
+
+            return pessoaData;
+        }
 
         public int Insert(long id, string nome, string telefone) {
-            using var connection = new SqlConnection("Server=localhost\\SQLEXPRESS; Database = ESTUDO1; Trusted_Connection = true");
-            var pessoaData = connection.Query<Pessoa>("SELECT * FROM PESSOA").ToList();
+            using var connection = new SqlConnection(_connectionString);
 
-            pessoaData.ForEach(el => Console.WriteLine(el.Nome));
-            return 9;
+            var query = "INSERT INTO PESSOA (id, nome, telefone) VALUES (@id, @nome, @telefone)";
+
+            var result = connection.Execute(query, new { id = id, telefone = telefone, nome = nome });
+
+            return result;
+        }
+
+        public int Update(long id, string nome, string telefone) {
+            using var connection = new SqlConnection(_connectionString);
+
+            var query = "UPDATE PESSOA SET nome = @nome, telefone = @telefone WHERE id = @id";
+
+            var result = connection.Execute(query, new { id = id, telefone = telefone, nome = nome });
+
+            return result;
+        }
+
+        public int Delete(long id, string nome, string telefone) {
+            using var connection = new SqlConnection(_connectionString);
+
+            var query = "DELETE FROM PESSOA WHERE id = @id";
+
+            var result = connection.Execute(query, new { id = id });
+
+            return result;
         }
 
     }
--- View/Program.cs
+++ View/Program.cs
@@ -5,39 +5,12 @@ using System;
 namespace hello_world_console_dotnet_core {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("oijaoisd");
+            //Console.WriteLine("oijaoisd");
             PessoaRepository rep = new PessoaRepository();
-            Console.WriteLine("asdasd");
-            rep.Insert(1, "9", "adjo");
+            //Console.WriteLine(rep.ListALL());
+           // rep.Insert(2, "9", "adjo");
+
+
         }
     }
 }
-/*
- 
-
-            while (true) {
-
-                Console.WriteLine("Digite o nome da pessoa: ");
-                string nome = Console.ReadLine();
-
-                Console.WriteLine("Digite o telefone da pessoa: ");
-                string telefone = Console.ReadLine();
-
-                Console.Clear();
-
-                Pessoa pessoa = new Pessoa(nome, telefone);
-
-                Console.WriteLine($"1. {nome} - {telefone}");
-
-                Console.WriteLine("\n\n\n----------------------\nVocê desesa adicionar outra pessoa? (S/n)? ");
-                string condicaoSaida = Console.ReadLine();
-
-                if(condicaoSaida == "N" || condicaoSaida == "n") {
-                    break;
-                }
-
-                Console.Clear();
-               
-            }
-        
- */
